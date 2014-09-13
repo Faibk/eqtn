f052a9f93e32a4fbccad7e33a5dd9b1f1135fde9
Fabian Kögel <f.koegel@web.de>
2014-09-13 23:38:22 +0200

    base functionality implemented, stable

--- js/main.js
+++ js/main.js
@@ -3,8 +3,18 @@ var app = {
 	init: function () {
 		this.model = new EqtStore();
         this.registerEvents();
+        this.registerHelper();
         this.route();
 	},
+    
+    registerHelper: function () {
+        Handlebars.registerHelper('times', function(n, block) {
+            var accum = '';
+            for(var i = 0; i < n; ++i)
+                accum += block.fn(i);
+            return accum;
+        });  
+    },
     
     registerEvents: function () {
         $(window).on('hashchange', this.route);
--- js/views/ingameView.js
+++ js/views/ingameView.js
@@ -1,12 +1,31 @@
 var IngameView = function () {
     
-    this.el = $("<div/>").attr("class", "view");
-    
-    this.render = function () {
-        this.el.html(IngameView.template(app.model.getEQT()));
-        return this.el;
+    this.init = function () {
+        this.eqtobject = app.model.getEQT();
+        this.el = $("<div/>").attr("class", "view");
+        this.el.on("change", "input[type=range]", this.recalculateBlocks);
+        this.el.on("change", "input[type=range]", $.proxy(function () {this.check()}, this));
     };
     
+    this.render = function () {
+        console.log(this.eqtobject);
+        this.el.html(IngameView.template(this.eqtobject));
+        return this.el;
+    };
+        
+    this.recalculateBlocks = function () {
+        var id = $(this).attr("id");
+        var height = $(this).val()*10+"px";
+        $(".block[data-slider-binding="+id+"]").each(function () {$(this).css("height", height)});
+    };
+    
+    this.check = function () {
+        var params = $("input[type=range]").map(function () {return parseInt($(this).val());}).get();
+        console.log(params);
+        if (this.eqtobject.eqt.apply(undefined, params)) alert("Gewonnen!");
+    };
+            
+    this.init();
 };
 
 IngameView.template = Handlebars.compile($("#ingame_tpl").html());
